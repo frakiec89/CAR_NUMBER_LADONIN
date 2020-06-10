5fe8efcfca1a85ebe7c6fdbbbe55176161e7c5c1
РИШАТ Ахтямов <rishat-rus@mail.ru>
2020-06-10 10:44:28 +0400

    Вынес  в  отдельные тесты

--- numbers_autoTests/AUTOTests.cs
+++ numbers_autoTests/AUTOTests.cs
@@ -14,25 +14,82 @@ namespace numbers_auto.Tests
         [TestMethod()]
         public void IsTryeNomberAutoTest()
         {
-                      
-            Assert.AreEqual( AUTO.IsTryeNomberAuto("А111ФФ163"), true);
-            Assert.AreEqual(AUTO.IsTryeNomberAuto("В111ФФ63"), true);
-            Assert.AreEqual(AUTO.IsTryeNomberAuto("А999ГГ163"), true);
-            Assert.AreEqual(AUTO.IsTryeNomberAuto("Б001ФФ01"), true);
-            Assert.AreEqual(AUTO.IsTryeNomberAuto("В111ФФ15"), true);
-            Assert.AreEqual(AUTO.IsTryeNomberAuto("А594ЧЧ63"), true);
-            Assert.AreEqual(AUTO.IsTryeNomberAuto("Р101РФ55"), true);
-            Assert.AreEqual(AUTO.IsTryeNomberAuto("111ФФ163"), false);
-            Assert.AreEqual(AUTO.IsTryeNomberAuto(""), false);
-            Assert.AreEqual(AUTO.IsTryeNomberAuto("А9999ГГ163"), false);
-         // Assert.AreEqual(AUTO.IsTryeNomberAuto("Б0F1ФФ01"), false);
-            Assert.AreEqual(AUTO.IsTryeNomberAuto("В111ФФ1555"), false);
-          //  Assert.AreEqual(AUTO.IsTryeNomberAuto("А59401163"), false);
-            Assert.AreEqual(AUTO.IsTryeNomberAuto("Р1PP163"), false);
-            
+              Assert.AreEqual( AUTO.IsTryeNomberAuto("А111ФФ163"), true);
         }
 
 
-      
+        [TestMethod()]
+        public void IsTryeNomberAutoTest1 ()
+        {
+            Assert.AreEqual(AUTO.IsTryeNomberAuto("В111ФФ63"), true);
+        }
+
+        [TestMethod()]
+        public void IsTryeNomberAutoTest2()
+        {
+            Assert.AreEqual(AUTO.IsTryeNomberAuto("А999ГГ163"), true);
+        }
+
+        [TestMethod()]
+        public void IsTryeNomberAutoTest3()
+        {
+            Assert.AreEqual(AUTO.IsTryeNomberAuto("Б001ФФ01"), true);
+        }
+
+        [TestMethod()]
+        public void IsTryeNomberAutoTest4()
+        {
+            Assert.AreEqual(AUTO.IsTryeNomberAuto("В111ФФ15"), true);
+        }
+
+        [TestMethod()]
+        public void IsTryeNomberAutoTest5()
+        {
+            Assert.AreEqual(AUTO.IsTryeNomberAuto("А594ЧЧ63"), true);
+        }
+
+        [TestMethod()]
+        public void IsTryeNomberAutoTest6()
+        {
+            Assert.AreEqual(AUTO.IsTryeNomberAuto("Р101РФ55"), true);
+        }
+
+        [TestMethod()]
+        public void IsTryeNomberAutoTest7()
+        { 
+            Assert.AreEqual(AUTO.IsTryeNomberAuto("111ФФ163"), false);
+        }
+
+        [TestMethod()]
+        public void IsTryeNomberAutoTest8()
+        {
+            Assert.AreEqual(AUTO.IsTryeNomberAuto("А9999ГГ163"), false);
+        }
+
+
+        [TestMethod()]
+        public void IsTryeNomberAutoTest9()
+        {
+            Assert.AreEqual(AUTO.IsTryeNomberAuto("В111ФФ1555"), false);
+        }
+
+        [TestMethod()]
+        public void IsTryeNomberAutoTest10()
+        {
+            Assert.AreEqual(AUTO.IsTryeNomberAuto("Р1PP163"), false);
+        }
+
+        [TestMethod()]
+        public void IsTryeNomberAutoTest11()
+        {
+            Assert.AreEqual(AUTO.IsTryeNomberAuto("Б0F1ФФ01"), false);
+        }
+
+        [TestMethod()]
+        public void IsTryeNomberAutoTest12 ()
+        {
+            Assert.AreEqual(AUTO.IsTryeNomberAuto("А59401163"), false);
+        }
+
     }
 }
